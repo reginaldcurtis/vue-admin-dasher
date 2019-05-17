e23e4e244ccea0e1c7f44dd4a18728ef5212e5ab
Reginald Curtis <Reginald.Curtis@gmail.com>
2019-05-17 10:37:01 -0700

    Styled first component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,4 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
+<template></template>
 
 <style lang="scss">
 @import "@/global-styles/colors.scss";
